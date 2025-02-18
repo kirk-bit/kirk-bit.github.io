8ce33aeab620a278c38d11d2f0d708c690a130f2
kirk-bit <killer.kirk71@gmail.com>
2025-02-18 14:31:11 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,16 @@
-body {
-    font-family: Arial, sans-serif;
-    background: #e0f2f1; /* Light blue-green for water theme */
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
-header {
-    background: #004d40; /* Dark green for nature feel */
-    color: white;
-}
-
-#home h1 {
-    text-align: center;
-}
-
-.gallery-images img {
-    width: 30%;
-    margin: 1%;
-}
-
-/* Additional styles for responsiveness, navigation, etc. */
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
